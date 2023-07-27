3147607e5ee2bc8c15a72b9ef85d6c4549ba8d9b
Meily123 <mei1234ly@gmail.com>
2023-07-27 17:50:37 +0700

    refactor: move routes to router

---
+++ src/router.ts
@@ -0,0 +1,13 @@
+import UserRouter from "./user/user.routes";
+import {errorHandler} from "./middleware/error/errorHandler";
+import VideoRouter from "./video/video.routes";
+import ProductRouter from "./product/product.routes";
+
+import express from "express";
+const router = express.Router();
+
+router.use("/user", UserRouter, errorHandler);
+router.use('/videos', VideoRouter, errorHandler);
+router.use("/products",ProductRouter, errorHandler);
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,12 @@ import {errorHandler} from "./middleware/error/errorHandler";
 require('dotenv').config();
 
 import connect from "./config/database/mongo";
-import express from "express"
+import express from "express";
+import router from "./router";
 const auth = require("./middleware/authentication/auth");
 
 const PORT = process.env.PORT;
 const app = express();
-
-import VideoRouter from './video/video.routes';
-import UserRouter from "./user/user.routes";
-import ProductRouter from "./product/product.routes";
 
 connect();
 
@@ -20,10 +17,7 @@ app.get("/welcome",auth, errorHandler, (req, res) => {
     res.status(200).send("Welcome 🙌 ");
 });
 
-app.use("/user", UserRouter, errorHandler);
-app.use('/videos', VideoRouter, errorHandler);
-app.use("/products",ProductRouter, errorHandler);
-
+app.use(router);
 
 app.listen(PORT, () => {
     console.log(`Server Started at ${PORT}`)
